345f3a0cf9c768a7977e672b96f17222b5d2fe36
Sam B <sam2b@uw.edu>
2018-07-24 12:02:17 -0700

    Slight formatting adjustment.

--- test.js
+++ test.js
@@ -1,14 +1,11 @@
 // ------------- Edit only these values below -------------
-var sensorPin = 33;         // Pin for cadence sensor.
-var buttonUpPin = 35;       // Pin button up.
-var buttonDownPin = 37;     // Pin button down.
+var sensorPin = 33; // Pin for cadence sensor.
+var buttonUpPin = 35; // Pin button up.
+var buttonDownPin = 37; // Pin button down.
 var buttonAutoModePin = 32;
-var resistanceUpPin = 5;    // GPIO5 resistance up.
-var resistanceDownPin = 6;  // GPIO6 resistance down.
-var startingLevel = 8;      // If too easy, increase this value.  Else, decrease.
-var zwiftUsername = "";
-var zwiftPassword = "";
-var zwiftID = "";
+var resistanceUpPin = 5; // GPIO5 resistance up.
+var resistanceDownPin = 6; // GPIO6 resistance down.
+var startingLevel = 9; // If too easy, increase this value.  Else, decrease.
 // -------------- Do not edit below this line --------------
 
 var keypress = require('keypress');
@@ -18,7 +15,6 @@ var power_meter = require('./power-meter');
 var pm = new power_meter.PowerMeter();
 var Zwifter = require("./Zwifter");
 var z = new Zwifter.Zwifter();
-//var levelAtZeroPercentGrade = z.getLevelAtZeroPercentGrade(); // TODO, remove this line.
 var autoMode = true;
 var easyMode = false;
 var counter = 0;
@@ -29,9 +25,9 @@ function initialize() {
     keypress(process.stdin);
 
     // listen for the "keypress" event
-    process.stdin.on('keypress', function (ch, key) {
+    process.stdin.on('keypress', function(ch, key) {
         //console.log('got "keypress"', key);
-        switch(key.name) {
+        switch (key.name) {
             case 'q': // ctrl-q
             case 'Q':
                 if (key && key.ctrl && (key.name == 'q' || key.name == 'Q')) {
@@ -42,7 +38,7 @@ function initialize() {
                 break;
             case 'e':
             case 'E':
-                if(easyMode) {
+                if (easyMode) {
                     easyMode = false;
                     startingLevel += 2; // Restore to original starting level value.
                     console.log("Easy mode OFF.");
@@ -56,7 +52,7 @@ function initialize() {
                 console.log("startingLevel = " + z.getLevelAtZeroPercentGrade());
                 break;
             case 'space':
-                if(autoMode) {
+                if (autoMode) {
                     autoMode = false;
                     console.log("Auto mode OFF."); // Manual control of the resistance.
                 } else {
@@ -86,6 +82,7 @@ function run() {
 
     // Runs asynchronously to prevent drops in RPM.
     setInterval(function() {
+        //console.log("Broadcasting power and cadence...");
         power_instant = Math.round(speedmeter.getPower());
         cadence = Math.round(speedmeter.getSpeed());
         /*if (isNaN(power_instant)) {
@@ -94,13 +91,20 @@ function run() {
         if (isNaN(cadence)) {
             console.log("------------cadence NAN----------------"); // debugging, remove me.
         }*/
-        power_instant = isNaN(power_instant) ? 0 : power_instant;  //avoid NaN
+        power_instant = isNaN(power_instant) ? 0 : power_instant; //avoid NaN
         pm.broadcast(power_instant, cadence);
     }, 249);
-    
+
     // Runs asynchronously to change the level. Most noticeable when suddenly encountering a steep grade, i.e. from 0% to 10%.
+    var levelValue;
     setInterval(function() {
-        if (autoMode) {
+        goodLevelValue = z.getLevelRequested();
+        if (!goodLevelValue) {
+            console.log("Problem getting levelRequested, likely due");
+            console.log("to an issue with the zwift-mobile-api");
+            console.log(" ");
+        }
+        if (autoMode && goodLevelValue) {
             setResistance(z.getLevelRequested(), speedmeter.getLevel());
         }
     }, 2000);
@@ -108,31 +112,33 @@ function run() {
 
 // Logic for relays.
 function setResistance(requestedLevel, currentLevel) {
+    //console.log("requestedLevel = " + requestedLevel);
+    //console.log("currentLevel   = " + currentLevel);
     var delta = requestedLevel - currentLevel;
     if (isNaN(delta)) delta = 0;
-    
+
     // Bound the delta.  Can be positive or negative.
     if ((delta + currentLevel) > speedmeter.getMaxLevel()) {
-        delta = speedmeter.getMaxLevel() - currentLevel;  // maximum of 18.
+        delta = speedmeter.getMaxLevel() - currentLevel;
     } else if ((delta + currentLevel) < 1) {
         delta = 1; // minimum of 1.
     }
-    
-    var relayDelay = 100;  // Too low of delay value might be debounced in SpeedMeter.
+
+    var relayDelay = 100; // Too low of delay value might be debounced in SpeedMeter.
     var i = delta;
-    if (i < 0) i*=-1; // i must always be positive.
-    
+    if (i < 0) i *= -1; // i must always be positive.
+
     // Conditions in order of highest frequency.
     if (delta == 0) {
         // nothing. Keep this code block for efficiency's sake.
     } else if (delta > 0) {
-       //console.log("   RAISING RESISTANCE " + i + " TIMES     ^^^^^^^^");
-       while (i > 0 && (z.getLevelRequested() <= speedmeter.getMaxLevel())) {
-           speedmeter.toggleRelay(resistanceUpPin, relayDelay);
-           speedmeter.incLevel();
-           i--;
+        //console.log("   RAISING RESISTANCE " + i + " TIMES     ^^^^^^^^");
+        while (i > 0 && (z.getLevelRequested() <= speedmeter.getMaxLevel())) {
+            speedmeter.toggleRelay(resistanceUpPin, relayDelay);
+            speedmeter.incLevel();
+            i--;
         }
-        console.log("    Level " + speedmeter.getLevel());
+        console.log("    Level " + speedmeter.getLevel() + " at " + z.getGrade() + "%");
     } else { //if (delta < 0) {
         //console.log("   vvvvvvvv    LOWERING RESISTANCE " + i + " TIMES");
         while (i > 0 && (speedmeter.getLevel() > 0)) {
@@ -140,7 +146,7 @@ function setResistance(requestedLevel, currentLevel) {
             speedmeter.decLevel();
             i--;
         }
-        console.log("    Level " + speedmeter.getLevel());
+        console.log("    Level " + speedmeter.getLevel() + " at " + z.getGrade() + "%");
     }
 }
 
